6370126b2b0049dcd8c8af516bfae706bc290307
mattygio <vannucc77@msn.com>
2016-09-18 17:36:31 -0400

    Matt's LED Control Program
    There are 3 different control styles in this code. It was designed for
    my 2 X 4 matrix.

--- dank_square/dank_square.ino
+++ dank_square/dank_square.ino
@@ -1,91 +1,45 @@
-#include "LedControl.h"
 
-LedControl lc1 = LedControl(0,1,13,1);
+#include <LedControl.h>
+#include <binary.h>
+
+LedControl lc1 = LedControl(12,11,10,1);
 //data pin 0
 //clock pin 1
 //csPin 13
 //1 device total
+int T = 500;
+
 void setup() {
-  // put your setup code here, to run once:
-  lc1.setIntensity(0,15);
+  lc1.setIntensity(0,10);
   lc1.clearDisplay(0);
   lc1.shutdown(0, false);
   
- //Test change on Matt's computer
- //2nd Test change on Matt's Computer
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  int T = 200;
-   while (1==1)
-   {
 
-    //square 1
-     lc1.setLed(0,3,4,true);
-     lc1.setLed(0,3,5,true);
-     lc1.setLed(0,4,4,true);
-     lc1.setLed(0,4,5,true);
-
-     lc1.setLed(0,0,0,true);
-     lc1.setLed(0,1,0,true);
-     lc1.setLed(0,0,7,true);
-
-     lc1.setLed(0,0,1,true);
-     lc1.setLed(0,1,1,true);
-     lc1.setLed(0,0,2,true);
-
-     lc1.setLed(0,7,0,true);
-     lc1.setLed(0,6,0,true);
-     lc1.setLed(0,7,7,true);
-
-     lc1.setLed(0,7,1,true);
-     lc1.setLed(0,6,1,true);
-     lc1.setLed(0,7,2,true);
-
+     // valid coordinates 00 01 02 03
+     //                   10 11 12 13
      
-     delay (T);
-     lc1.clearDisplay(0);
+//     for(int i = 0; i < 2; i++){   //Lights all the LEDs in a row
+//       for(int j = 0; j < 4;j++){
+//         lc1.setLed(0,i,j,true);
+//         delay(T);
+////         lc1.setLed(0,i,j,false); 
+//       }
+//     } 
+//     
+//     for(int i = 0; i < 2; i++){ //Turns off all the LEDs in a row
+//       for(int j = 0; j < 4;j++){
+//         lc1.setLed(0,i,j,false);
+//         delay(T); 
+//       }
+//     } 
 
-   //square 2
-
-     lc1.setLed(0,2,3,true);
-     lc1.setLed(0,2,4,true);
-     lc1.setLed(0,2,5,true);
-     lc1.setLed(0,2,6,true);
-
-     lc1.setLed(0,3,3,true);
-     lc1.setLed(0,4,3,true);
-     lc1.setLed(0,5,3,true);
-
-     lc1.setLed(0,5,4,true);
-     lc1.setLed(0,5,5,true);
-     lc1.setLed(0,5,6,true);
-
-     lc1.setLed(0,3,6,true);
-     lc1.setLed(0,4,6,true);
-
-     //borders
-
-     lc1.setLed(0,1,7,true);
-     lc1.setLed(0,2,7,true);
-     lc1.setLed(0,1,6,true);
-
-     lc1.setLed(0,1,2,true);
-     lc1.setLed(0,2,2,true);
-     lc1.setLed(0,1,3,true);
-
-     lc1.setLed(0,6,7,true);
-     lc1.setLed(0,5,7,true);
-     lc1.setLed(0,6,6,true);
-
-     lc1.setLed(0,6,2,true);
-     lc1.setLed(0,5,2,true);
-     lc1.setLed(0,6,3,true);
-
-     delay (T);
-     lc1.clearDisplay(0);
-
-     
-   }
+     lc1.setRow(0,0,B10100000); //alternates the LEDs in a row
+     lc1.setRow(0,1,B10010000);
+     delay(T);
+     lc1.setRow(0,0,B01010000);
+     lc1.setRow(0,1,B01100000);
+     delay(T);
 }
